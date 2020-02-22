1d7897adcf2a1bd3fa254856f9cbe3fc84632ddd
JasonKieferEE <Jason.Kiefer@outlook.com>
2020-02-22 16:51:18 -0500

    Isolated selenium code

--- Selenium.js
+++ Selenium.js
@@ -1,9 +1,13 @@
+
+//Selenium example with JSSoup
+
 const { Builder, By, Key, until } = require('selenium-webdriver');
 const selenium = require('selenium-webdriver');
-let chrome = require('selenium-webdriver/chrome');
-let chromedriver = require('chromedriver');
-let gecko = require('geckodriver');
-let JSSoup = require('jssoup').default;
+const chrome = require('selenium-webdriver/chrome');
+const chromedriver = require('chromedriver');
+const JSSoup = require('jssoup').default;
+
+require('geckodriver');
 
 chrome.setDefaultService(new chrome.ServiceBuilder(chromedriver.path).build());
 
@@ -25,11 +29,7 @@ async function scrape() {
 
         driver.getPageSource()
         .then(async page => {
-            let html = new JSSoup(page).findAll('p');
-            trie.createWord('cheese');
-            for (let i =0 ; i < 3 ; i++)
-                trie.insertDefinition('cheese', html[i].text.trim());
-            trie.traverse('cheese');
+            let pTags = new JSSoup(page).findAll('p');
             driver.findElements(By.xpath('//*[@class="mw-parser-output"]//a'))
             .then(aTags => {
                 //aTags[1].click();
@@ -41,94 +41,6 @@ async function scrape() {
         })
     }
     finally {
-        //driver.quit();
+        driver.quit();
     }
 };
-
-class LinkedList {
-    constructor() {
-        this.head = null;
-    }
-}
-
-class Definition {
-    constructor(description = null) {
-        this.description = description;
-        this.next = null;
-    }
-}
-
-class Word {
-    constructor() {
-        this.definitions = new LinkedList();
-    }
-}
-
-class Trie {
-    constructor() {
-        this.root = {}
-    }
-
-    insertList(list) {
-        list.forEach(el => {
-            return this.insert(el);
-        })
-    }
-    
-    createWord(word, root = this.root) {
-        if(!root)
-            this.root = root;
-        [...word].forEach(el => {
-            if (!root[el])
-                root[el] = new Word();
-            root = root[el]
-        })
-        root = new Word();
-    }
-
-    insertDefinition(word, description, root = this.root) {
-        [...word].forEach(el => {
-            if (!root[el])
-                return null;
-            root = root[el]
-        })
-        
-        let node = root.definitions.head;
-        
-        if (!node) {
-            root.definitions.head = new Definition(description);
-            return;
-        }
-        
-
-        while(node.next) 
-            node = node.next;
-        node.next = new Definition(description);
-    }
-
-    traverse(value, root = this.root) {
-
-        process.stdout.write('Searching for \'' + value + '\': \n');
-
-        [...value].forEach(el => {
-            root = root[el]
-        })
-
-        let head = root.definitions.head
-
-        while(head) {
-            head 
-                ? process.stdout.write(head.description.toString() + '\n\n')
-                : process.stdout.write('No definitions found\n');
-            head = head.next;
-        }
-    }
-}
-
-let trie = new Trie();
-
-function main() {
-    scrape();
-}
-
-main();
